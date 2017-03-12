84398b6d072099d7b8659aad37076c88fd88d367
FrankRx41 <frank_rx_41@gmail.com>
2017-03-12 15:05:15 +0800

    完成了debug代碼

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <windows.h>
 
+#define debug(x)    printf("%s(%d): "#x" = %08X\n",__FUNCTION__,__LINE__,x)
+
 int main(){
+    int x = 1;
     printf("hello World\n");
+    debug(x);
     return 0;
 }
